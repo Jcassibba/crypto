12e04f687b0b2fbb2b32c6e34472cfedef40662e
Jcassibba <46285888+Jcassibba@users.noreply.github.com>
2022-03-06 10:08:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ from dotenv import load_dotenv
 project_folder = os.path.expanduser('~/my-project-dir')  
 load_dotenv(os.path.join(project_folder, '.env'))
 
-from flask import Flask
+from flask import Flask, render_template
 import main
 
 app = Flask(__name__)
 
 @app.route("/")
 def ready():
-    #print("bot is ready")
+    print("bot is ready")
     ##main code file##
     crypto = main()
-    #return 
+    return render_template('index.html') 
     crypto.run()  
 
 if (__name__ == "__main__"):
