3c84a66e1a07d8859e6f044bdd2737c756e097c3
Jcassibba <46285888+Jcassibba@users.noreply.github.com>
2022-03-08 16:42:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,9 @@ for j in range (1000):
     
     while d == 1:
       ###############################################################connect to database
-      conn = psycopg2.connect("databasename=ddt3jth60iul84 user=lmhhwkksgpwqbs password=bfc5d49a5575e869e5a39f3cc9d9bc79585a283dfa5189b6fa37a0d30ed6e11d, host=ec2-54-157-160-218.compute-1.amazonaws.com, port=5432")
-      dbt = CREATE TABLE coin(headings_list) #invalid syntax
+      conn = psycopg2.connect("dbname=ddt3jth60iul84, user=lmhhwkksgpwqbs,\ 
+                              password=bfc5d49a5575e869e5a39f3cc9d9bc79585a283dfa5189b6fa37a0d30ed6e11d, host=ec2-54-157-160-218.compute-1.amazonaws.com, port=5432")
+      dbt = CREATE TABLE coin(now,headings_list) #invalid syntax
       d += 1
       ##########################################################################close database
       conn.close()
@@ -48,7 +49,8 @@ for j in range (1000):
       d = str(coin)
       n = str(now)
       #################################################################### connect to data base
-      conn=psycopg2.connect("databasename=ddt3jth60iul84, user=lmhhwkksgpwqbs, password=bfc5d49a5575e869e5a39f3cc9d9bc79585a283dfa5189b6fa37a0d30ed6e11d, host=ec2-54-157-160-218.compute-1.amazonaws.com, port=5432")
+      conn=psycopg2.connect("dbname=ddt3jth60iul84, user=lmhhwkksgpwqbs, \
+      password=bfc5d49a5575e869e5a39f3cc9d9bc79585a283dfa5189b6fa37a0d30ed6e11d, host=ec2-54-157-160-218.compute-1.amazonaws.com, port=5432")
       #################################################################### add values to table
       db = INSERT INTO dbt(heading_list) VALUES (n,d)
       conn.cursor.execute(db)
