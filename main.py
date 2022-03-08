12d7ee9931c70010f227acdb272ae3b3d296019e
Jcassibba <46285888+Jcassibba@users.noreply.github.com>
2022-03-08 16:15:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from bs4 import BeautifulSoup
 import time 
 import datetime
 
+#import psycopg2
 
 x=1
+#d=1
 
 for j in range (1000):
   while(x==1):
@@ -21,7 +23,15 @@ for j in range (1000):
     for x in headings:
       headings_list.append(x.text)
     headings_list = headings_list[:6]
-    #print (headings_list)
+    
+    #while d=1:
+      ###############################################################connect to database
+      #conn=psycopg2.connect("databasename=? user=? password=?")
+      #CREATE TABLE coin(headings_list)
+      #d +=1
+      ##########################################################################close database
+      #conn.close()
+      ##########################################################################
   
     data = []
   
@@ -37,13 +47,21 @@ for j in range (1000):
     for coin in data: 
       d = str(coin)
       n = str(now)
-      ###### write to database
-      execute_query(connection,n,d)
-      #crypto.write(n)
-      #crypto.write(d)
+      #################################################################### connect to data base
+      #conn=psycopg2.connect("databasename=? user=? password=?")
+      #################################################################### add values to table
+      #data="INSERT INTO coin_table(heading_list) VALUES (n,d)"
+      #conn.cursor.execute(data)
+      ######crypto.write(n) [remove]
+      ######crypto.write(d) [remove]
+      ##########################################################################close database
+      #cursor.close()
+      #conn.commit()
+      #conn.close()
+      ##########################################################################
       print(coin)
       print('')
-    crypto.close()
+    #crypto.close() [remove]
   time.sleep(3600)
   x=1
 #################################
