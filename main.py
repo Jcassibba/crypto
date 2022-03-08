9c12fa66164035208fe9469c1838d3551e7ac5ed
Jcassibba <46285888+Jcassibba@users.noreply.github.com>
2022-03-07 19:17:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,63 @@
-
-##################### BOT ###########
+################### BOT ###########
 import requests
 from bs4 import BeautifulSoup
 import time 
-from datetime import *
+import datetime
 
+def create_db_connection(ec2-54-157-160-218.compute-1.amazonaws.com, lmhhwkksgpwqbs, bfc5d49a5575e869e5a39f3cc9d9bc79585a283dfa5189b6fa37a0d30ed6e11d, ddt3jth60iul84):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
 
+    return connection
 
-#while(x==1): #have loop run once every 60 mins
+x=1
+
 for j in range (1000):
-  
-  crypto = open("crypto.json", "a")
-  now = datetime.now()
+  while(x==1):
+    crypto = open("crypto.json", "a")
+    now = datetime.datetime.now()
         
-  url = 'https://coinmarketcap.com/'
-  response = requests.get(url)
-  text = response.text
-  html_data = BeautifulSoup(text, 'html.parser')
-  headings = html_data.find_all('tr')[0]
-  headings_list = []
-  for x in headings:
-    headings_list.append(x.text)
-  headings_list = headings_list[:6]
-  #print (headings_list)
+    url = 'https://coinmarketcap.com/'
+    response = requests.get(url)
+    text = response.text
+    html_data = BeautifulSoup(text, 'html.parser')
+    headings = html_data.find_all('tr')[0]
+    headings_list = []
+    for x in headings:
+      headings_list.append(x.text)
+    headings_list = headings_list[:6]
+    #print (headings_list)
   
-  data = []
+    data = []
   
-  for x in range(1, 11):
-    row = html_data.find_all('tr')[x]
-    column_value = row.find_all('td')
-    dict = {}
+    for x in range(1, 11):
+      row = html_data.find_all('tr')[x]
+      column_value = row.find_all('td')
+      dict = {}
           
-    for i in range(6):
-      dict[headings_list[i]] = column_value[i].text
-    data.append(dict)
+      for i in range(6):
+        dict[headings_list[i]] = column_value[i].text
+      data.append(dict)
           
-  for coin in data: 
-    d = str(coin)
-    n = str(now)
-    ###### write to database
-    crypto.write(n)
-    crypto.write(d)
-    crypto.write("\n")
-    print(coin)
-    print('')
-    time.sleep(10)
-  crypto.close()
-  #x=1
+    for coin in data: 
+      d = str(coin)
+      n = str(now)
+      ###### write to database
+      execute_query(connection,n,d)
+      #crypto.write(n)
+      #crypto.write(d)
+      print(coin)
+      print('')
+    crypto.close()
+  time.sleep(10)
+  x=1
 #################################
