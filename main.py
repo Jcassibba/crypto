404bb07ab975bd8f5d3aa743ff453efc28fc8632
Jcassibba <46285888+Jcassibba@users.noreply.github.com>
2022-03-08 19:48:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ for j in range (1000):
       conn=psycopg2.connect("dbname=ddt3jth60iul84, user=lmhhwkksgpwqbs, \
       password=bfc5d49a5575e869e5a39f3cc9d9bc79585a283dfa5189b6fa37a0d30ed6e11d, host=ec2-54-157-160-218.compute-1.amazonaws.com, port=5432")
       #################################################################### add values to table
-      db = INSERT INTO dbt(heading_list) VALUES (n,d) #invalid syntex
+      db = """INSERT INTO dbt(heading_list) VALUES (n,d)""" #invalid syntex
       conn.cursor.execute(db)
       ######crypto.write(n) [remove]
       ######crypto.write(d) [remove]
