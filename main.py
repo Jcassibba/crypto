cfd35b81135f2c2d373ad7736b39cd0f7aef4879
Jcassibba <46285888+Jcassibba@users.noreply.github.com>
2022-03-13 19:49:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -46,10 +46,9 @@ for j in range (1000):
       d = str(coin)
       n = str(now)
       #################################################################### connect to data base
-      conn=psycopg2.connect("dbname=ddt3jth60iul84, user=lmhhwkksgpwqbs, \
-      password=bfc5d49a5575e869e5a39f3cc9d9bc79585a283dfa5189b6fa37a0d30ed6e11d, host=ec2-54-157-160-218.compute-1.amazonaws.com, port=5432")
+      conn=psycopg2.connect("dbname=ddt3jth60iul84, user=lmhhwkksgpwqbs, password=bfc5d49a5575e869e5a39f3cc9d9bc79585a283dfa5189b6fa37a0d30ed6e11d, host=ec2-54-157-160-218.compute-1.amazonaws.com, port=5432")
       #################################################################### add values to table
-      db = """INSERT INTO dbt(heading_list) VALUES (n,d)""" #invalid syntex
+      db == INSERT INTO dbt(heading_list) VALUES (n,d)
       conn.cursor.execute(db)
       ######crypto.write(n) [remove]
       ######crypto.write(d) [remove]
@@ -61,6 +60,6 @@ for j in range (1000):
       print(coin)
       print('')
     #crypto.close() [remove]
-  time.sleep(3600)
+  time.sleep(10)
   x=1
 #################################
