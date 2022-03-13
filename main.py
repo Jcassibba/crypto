4f736c45375a03eceec0374d8da4e1aa2d5cfa32
Jcassibba <46285888+Jcassibba@users.noreply.github.com>
2022-03-13 19:55:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ for j in range (1000):
       #################################################################### connect to data base
       conn=psycopg2.connect("dbname=ddt3jth60iul84, user=lmhhwkksgpwqbs, password=bfc5d49a5575e869e5a39f3cc9d9bc79585a283dfa5189b6fa37a0d30ed6e11d, host=ec2-54-157-160-218.compute-1.amazonaws.com, port=5432")
       #################################################################### add values to table
-      db == insert into coin(heading_list) VALUES (n,d) #invalid syntax
+      db == insert coin(heading_list) VALUES (n,d) #invalid syntax
       conn.cursor.execute(db)
       ######crypto.write(n) [remove]
       ######crypto.write(d) [remove]
