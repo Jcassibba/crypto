7335f42c28ee18ac6c943fa5c237f1da4d5177bc
Jcassibba <46285888+Jcassibba@users.noreply.github.com>
2022-03-13 20:09:20 -0400

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -4,7 +4,7 @@ from config import config
   
 def create_tables():
    
-    commands = CREATE TABLE coin (now, heading_listing)
+    commands = CREATE TABLE coin (now, heading_listing) #invalid syntax
     # connect to the PostgreSQL server
     conn = psycopg2.connect("dbname=ddt3jth60iul84, user=lmhhwkksgpwqbs, password=bfc5d49a5575e869e5a39f3cc9d9bc79585a283dfa5189b6fa37a0d30ed6e11d, host=ec2-54-157-160-218.compute-1.amazonaws.com, port=5432")
     cur = conn.cursor()
