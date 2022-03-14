6bf018ceba4d87ae820d8c9a94819525415fd10b
Jcassibba <46285888+Jcassibba@users.noreply.github.com>
2022-03-13 20:07:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ for j in range (1000):
     
     while d == 1:
       ###############################################################connect to database and create table
-      create_table.run()
+      #create_table.run()
       d += 1
       ##########################################################################
   
@@ -45,21 +45,21 @@ for j in range (1000):
     for coin in data: 
       d = str(coin)
       n = str(now)
-      #################################################################### connect to data base
-      conn=psycopg2.connect("dbname=ddt3jth60iul84, user=lmhhwkksgpwqbs, password=bfc5d49a5575e869e5a39f3cc9d9bc79585a283dfa5189b6fa37a0d30ed6e11d, host=ec2-54-157-160-218.compute-1.amazonaws.com, port=5432")
+      #################################################################### connect to database
+      #conn=psycopg2.connect("dbname=ddt3jth60iul84, user=lmhhwkksgpwqbs, password=bfc5d49a5575e869e5a39f3cc9d9bc79585a283dfa5189b6fa37a0d30ed6e11d, host=ec2-54-157-160-218.compute-1.amazonaws.com, port=5432")
       #################################################################### add values to table
-      db = INSERT INTO coin (now, heading_list) VALUES (n, d) #invalid syntax
-      conn.cursor.execute(db)
+      #db = INSERT INTO coin (now, heading_list) VALUES (n, d) #invalid syntax
+      #conn.cursor.execute(db)
       ######crypto.write(n) [remove]
       ######crypto.write(d) [remove]
       ##########################################################################close database
-      cursor.close()
-      conn.commit()
-      conn.close()
+      #cursor.close()
+      #conn.commit()
+      #conn.close()
       ##########################################################################
-      print(coin)
+      print(now+""+coin)
       print('')
     #crypto.close() [remove]
-  time.sleep(20)
+  time.sleep(10)   #(3600)
   x=1
 #################################
